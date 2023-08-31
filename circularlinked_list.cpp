efd38bc4038dcdb4858f17063c108c93a4c4ca4f
ispranaypink <117278249+ispranaypink@users.noreply.github.com>
2023-08-31 18:23:55 +0530

    Add files via upload

---
+++ circularlinked_list.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node {
+    public:
+    int data;
+    Node* next;
+};
+void push (Node** head_ref, int data){
+    Node* ptr1 = new Node();
+    `
+}
